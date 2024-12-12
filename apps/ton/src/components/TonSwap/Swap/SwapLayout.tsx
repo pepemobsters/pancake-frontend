6ff0f66abab98b169d39597013fbaba6b420528b
Chef Penguin <penguin@pancakeswap.com>
2024-12-12 13:08:50 +0530

    chore: remove all of old Swap widget

--- apps/ton/src/components/TonSwap/Swap/SwapLayout.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react'
-import { SwapFeaturesProvider } from './SwapFeaturesContext'
-
-function SwapLayout({ children }: { children: ReactNode }) {
-  return <SwapFeaturesProvider>{children}</SwapFeaturesProvider>
-}
-
-export default SwapLayout
