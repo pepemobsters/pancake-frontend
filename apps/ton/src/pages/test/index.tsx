6280a84eab8a7e985fa3cf9f909fa50d23e5e064
chef-ryan <ryan@pancakeswap.com>
2024-12-17 14:26:42 +0800

    feat: 🎸 Add connect/disconnection button

--- apps/ton/src/pages/test/index.tsx
+++ apps/ton/src/pages/test/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@pancakeswap/uikit'
+import { useTonConnectModal, useTonConnectUI } from '@tonconnect/ui-react'
 import { useAtomValue } from 'jotai'
 import { addressAtom } from 'ton/atom/addressAtom'
 import { isConnectedAtom } from 'ton/atom/isConnectedAtom'
@@ -9,16 +11,35 @@ export default () => {
   const address = useAtomValue(addressAtom)
   const balanceOfUSDC = useAtomValue(balanceOfAtom(TonContractNames.USDC))
   const tonBalance = useAtomValue(balanceOfAtom(TonContractNames.NATIVE))
+  const [ui] = useTonConnectUI()
+  const modal = useTonConnectModal()
   return (
     <div
       style={{
         padding: '20px',
       }}
     >
-      <p>Connected: {isConnected ? 'connected' : 'disconnected'}</p>
       <p>Address: {address || '-'}</p>
       <p>Ton Balance: {`${tonBalance}` || '-'}</p>
       <p>USDC Balance: {`${balanceOfUSDC}` || '-'}</p>
+      {isConnected && (
+        <Button
+          onClick={() => {
+            ui.disconnect()
+          }}
+        >
+          Disconnect
+        </Button>
+      )}
+      {!isConnected && (
+        <Button
+          onClick={() => {
+            modal.open()
+          }}
+        >
+          Connect
+        </Button>
+      )}
     </div>
   )
 }
